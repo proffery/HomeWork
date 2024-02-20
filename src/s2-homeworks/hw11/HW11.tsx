1c94a3bf8712d220d15930a5916c4063201978b7
Dmitry Shamko <proffery@gmail.com>
2024-02-20 13:00:17 +0300

    HW11 is Nonsense!

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -17,6 +17,12 @@ function HW11() {
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if (value instanceof Array) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(Number(value))
+        }
     }
 
     return (
@@ -30,15 +36,18 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            value={value1}
+                            onChange={change}
                         />
+                        <span id={'hw11-value-2'} style={{opacity: 0}} className={s.number}>{'100'}</span>
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            value={[value1, value2]}
+                            onChange={change}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,9 +4,44 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
+            sx={ // стили для слайдера // пишет студент
+                {
+                    color: '#00CC22',
+                    height: 3,
+                    padding: '13px 0',
+                    '& .MuiSlider-thumb': {
+                      height: 18,
+                      width: 18,
+                      backgroundColor: '#fff',
+                      border: '1px solid currentColor',
+                      '&:after': {
+                        content: '""',
+                        position:'absolute',
+                        height: '6px',
+                        width: '6px',
+                        borderRadius: '50% 50%',
+                        backgroundColor: '#00CC22',
+                      },
+                      '&:hover': {
+                        boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)',
+                      },
+                      '& .airbnb-bar': {
+                        height: 9,
+                        width: 1,
+                        backgroundColor: 'currentColor',
+                        marginLeft: 1,
+                        marginRight: 1,
+                      },
+                    },
+                    '& .MuiSlider-track': {
+                      height: 4,
+                    },
+                    '& .MuiSlider-rail': {
+                      color: '#8B8B8B',
+                      height: 4,
+                    },
+                  }
+            }
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
